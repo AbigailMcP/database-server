26e0b85ddc78c3ac9912cef4774e5c1b6dacb1a7
Abigail <abigail.mcphillips@gmail.com>
2016-10-17 11:55:02 +0100

    sets up set route

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+var database = {};
 
 app.get('/', function(req, res) {
   res.send('Hello world!');
+});
+
+app.get('/set', function(req, res) {
+  res.send(req.query);
 });
 
 app.listen(4000, function() {
--- test/serverSpec.js
+++ test/serverSpec.js
@@ -4,12 +4,26 @@ var app = require("../app");
 
 describe('Database server', function() {
 
-  var url = "http://localhost:4000/";
+  describe('homepage', function() {
+    var url = "http://localhost:4000/";
 
-  it('returns status 200', function(done) {
-    request(url, function(error, response, body) {
+    it('returns status 200', function(done) {
+      request(url, function(error, response, body) {
       expect(response.statusCode).to.equal(200);
       done();
+      });
     });
   });
+
+  describe('setting route', function() {
+    var url = "http://localhost:4000/set?somekey=somevalue";
+
+    it('returns status 200', function(done) {
+      request(url, function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+      });
+    });
+  });
+
 });
